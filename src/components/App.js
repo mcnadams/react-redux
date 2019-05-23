0055b78b5d4abc6019cce3c29a75190d556c77c8
mcnadams <mcneilbo@oregonstate.edu>
2019-05-23 16:17:50 -0700

    it does something  I think

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 import AllPosts from '../containers/AllPosts';
 import CreatePost from '../containers/CreatePost';
 
 export default function App() {
   return (
-    <>
-      <AllPosts />
-      <CreatePost />
-    </>
+    <Router>
+      <Route path='/' component={AllPosts} />
+      <Route path='/createPost' component={CreatePost} />
+    </Router>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,21 +1,13 @@
-/* eslint-disable no-console */
-import { createStore } from 'redux';
-import { ADD_POST, REMOVE_POST, addPost, removePost } from './actions/postActions';
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+import App from './components/App';
 
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
-const initialState = {
-  post: []
-};
-
-function reducer(state = initialState, action) {
-  switch(action.type) {
-    case ADD_POST:
-      return { ...state, post: action.payload };
-    case REMOVE_POST:
-      return { post: [] };
-    default:
-      return state;
-  }
-}
-
-export const store = createStore(reducer);
